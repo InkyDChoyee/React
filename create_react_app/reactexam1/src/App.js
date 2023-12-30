c99a7716b70d689925e0cac3a3563ddcf5dee133
InkyDChoyee <choyee0421@naver.com>
2023-12-30 13:11:36 +0900

    feat: jsx_grammar

--- create_react_app/reactexam1/src/App.js
+++ create_react_app/reactexam1/src/App.js
@@ -1,27 +1,24 @@
 import './App.css';
 
+import MyHeader from './MyHeader';
+
+import React from "react";
+
 function App() {
 
   let name = "이순신";
 
   return (
-    <div className="App">
+    // <div className="App">
+    // <React.Fragment>
+    <>
+      <MyHeader />
       <header className="App-header">
         <h2>안녕 리액트 {name}</h2>
-        {/*<img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
       </header>
-    </div>
+    </>
+    // </React.Fragment>
+    // </div>
   );
 }
 
