f528354a264b2908cd52b8eba5b8e7e156b485ce
InkyDChoyee <choyee0421@naver.com>
2024-01-09 18:19:24 +0900

    feat: using_areEqual

--- simplediary/src/OptimizeTest.js
+++ simplediary/src/OptimizeTest.js
@@ -41,13 +41,27 @@ const CounterB = ({ obj }) => {
   return <div>{obj.count}</div>;
 };
 
+// areEqual함수를 사용하여 얕은 비교가 아닌
+// 값을 비교하도록 해서 연산의 낭비를 하지 않도록 해줄 수 있다
 const areEqual = (prevProps, nextProps) => {
+  // 이전 props와 현재 props가 같은 경우 true를 반환
+  // = re-rendering을 하지 않는다
   if (prevProps.obj.count === nextProps.obj.count) {
     return true;
   }
+  // false인 경우 re-rendering을 일으킨다
   return false;
+
+  // = 식 간단화 하기
+  // = return prevProps.obj.count === nextProps.obj.count;
+  // 한줄로 작성해도 같은 기능을 한다
 };
 
+// 새로운 component 생성
+// areEqual는 React.memo의 비교함수로써 작용을 하게 된다
+// CounterB는 areEqual의 판단에 따라 re-render를 할지 말지 결정하게 되는
+// 메모화된 component가 된다
+// MemoizedCounterB 로 고차원이 된 함수를 받는다
 const MemoizedCounterB = React.memo(CounterB, areEqual);
 
 // OptimizeTest component 생성
