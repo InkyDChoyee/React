12b34859ed1d31bb3b0a7a32af6ceac9377a0b1d
InkyDChoyee <choyee0421@naver.com>
2024-01-22 17:16:09 +0900

    Feat: query_string_searchParams

--- emotion_diary/src/pages/Edit.js
+++ emotion_diary/src/pages/Edit.js
@@ -1,8 +1,24 @@
+import { useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+  // Query String 꺼내서 사용하기 = 비구조화 할당으로 사용
+  // useSearchParams() = query string을 처리하는 custom hook
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get("id");
+  console.log("id: ", id);
+
+  const mode = searchParams.get("mode");
+  console.log("mode: ", mode);
+
   return (
     <div>
       <h1>Edit</h1>
       <p>이 곳은 일기 수정 페이지입니다</p>
+      {/* setSearchParams를 바꾸는 버튼(객체를 전달한다) */}
+      <button onClick={() => setSearchParams({ who: "name" })}>
+        QS 바꾸기
+      </button>
     </div>
   );
 };
