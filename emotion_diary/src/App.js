53536799fe61f51f3fd15e57314c85a198978f5f
InkyDChoyee <choyee0421@naver.com>
2024-01-22 12:16:58 +0900

    Feat: use_Router, import_pages

--- emotion_diary/src/App.js
+++ emotion_diary/src/App.js
@@ -1,10 +1,33 @@
 import "./App.css";
+// 브라우저 url과 react app을 연결하는 기능
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// 페이지들을 import 해준다
+import Home from "./pages/Home.js"; // .js 생략 가능
+import New from "./pages/New";
+import Edit from "./pages/Edit";
+import Diary from "./pages/Diary";
+
+import RouteTest from "./components/RouteTest.js";
 
 function App() {
   return (
-    <div className="App">
-      <h2>App.js</h2>
-    </div>
+    // BrowserRouter로 감싸준다
+    // = 감싸져있는 부분은 브라우저 url과 매핑될 수 있다
+    <BrowserRouter>
+      <div className="App">
+        <h2>App.js</h2>
+        <Routes>
+          {/* 페이지 경로 매핑 */}
+          {/* path="/"  == path="/index" */}
+          <Route path="/" element={<Home />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/edit" element={<Edit />} />
+          <Route path="/diary" element={<Diary />} />
+        </Routes>
+        <RouteTest />
+      </div>
+    </BrowserRouter>
   );
 }
 
